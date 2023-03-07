f08afacdcc239d56a9aa8789fb214bf92952406b
danmaevsky <dan.mayevskiy@gmail.com>
2023-03-07 11:43:05 -0500

    Refactored the names of some variables to make food.js easier to read

--- API Gateway/routes/profile.js
+++ API Gateway/routes/profile.js
@@ -1,46 +1,47 @@
 require("dotenv").config();
 const express = require("express");
 const FITNESS_PORT = process.env.FITNESS_PORT;
+const ACCOUNT_PORT = process.env.ACCOUNT_PORT;
 
 const router = express.Router();
-const fitnessURL = `http://localhost:${FITNESS_PORT}/food`;
-let fitnessRequest;
-let fitnessResponse;
+const foodURL = `http://localhost:${FITNESS_PORT}/food`;
+const authURL = `http://localhost:${ACCOUNT_PORT}/auth`;
+let foodRequest;
+let foodResponse;
 
 /* Get Food by ID */
 router.get("/:foodId", async (request, response) => {
-	if (request.params.foodId) {
-		fitnessRequest = `${fitnessURL}/${request.params.foodId}`;
-		fitnessResponse = await fetch(fitnessRequest, {
-			method: "GET",
-		}).then((response) => response.json());
-	}
-	// console.log(fitnessResponse);
-	response.send(fitnessResponse);
+    if (request.params.foodId) {
+        foodRequest = `${foodURL}/${request.params.foodId}`;
+        foodResponse = await fetch(foodRequest, {
+            method: "GET",
+        }).then((response) => response.json());
+    }
+    response.send(foodResponse);
 });
 
 /* Get Food by Search, Barcode, or User ID */
 router.get("/", async (request, response) => {
-	if (request.query.search) {
-		fitnessRequest = `${fitnessURL}/?search=${request.query.search}`;
-		fitnessResponse = await fetch(fitnessRequest, {
-			method: "GET",
-		}).then((response) => response.json());
-	} else if (request.query.barcode) {
-		// We could authenticate?
-		fitnessRequest = `${fitnessURL}/?barcode=${request.query.barcode}`;
-		fitnessResponse = await fetch(fitnessRequest, {
-			method: "GET",
-		}).then((response) => response.json());
-	} else if (request.query.userId) {
-		// We will do user authentication to prevent client from making this request unless it is their own account
-		fitnessRequest = `${fitnessURL}/?userId=${request.query.userId}`;
-		fitnessResponse = await fetch(fitnessRequest, {
-			method: "GET",
-		}).then((response) => response.json());
-	}
+    if (request.query.search) {
+        foodRequest = `${foodURL}/?search=${request.query.search}`;
+        foodResponse = await fetch(foodRequest, {
+            method: "GET",
+        }).then((response) => response.json());
+    } else if (request.query.barcode) {
+        // We could authenticate?
+        foodRequest = `${foodURL}/?barcode=${request.query.barcode}`;
+        foodResponse = await fetch(foodRequest, {
+            method: "GET",
+        }).then((response) => response.json());
+    } else if (request.query.userId) {
+        // We will do user authentication to prevent client from making this request unless it is their own account
+        foodRequest = `${foodURL}/?userId=${request.query.userId}`;
+        foodResponse = await fetch(foodRequest, {
+            method: "GET",
+        }).then((response) => response.json());
+    }
 
-	response.send(fitnessResponse);
+    response.send(foodResponse);
 });
 
 module.exports = router;
