171697fc31d4f7dc490d76b599d21fb76a86539d
danmaevsky <dan.mayevskiy@gmail.com>
2023-03-13 18:58:49 -0400

    Added newToken method

--- routes/account.js
+++ routes/account.js
@@ -49,6 +49,21 @@ router.post("/login", async (request, response) => {
 	response.send(authResponse);
 });
 
+/* GET a new access token given a refresh token */
+router.get("/newToken", async (request, response) => {
+	authRequest = `${AUTH_URL}/newToken`;
+	authResponse = await fetch(authRequest, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(request.body),
+	}).then((res) => {
+		response.status(res.status);
+		return res.json();
+	});
+
+	response.send(authResponse);
+});
+
 /* Put to Change Password */
 router.put("/changePassword", util.AuthTokenMiddleware, async (request, response) => {
 	authRequest = `${AUTH_URL}/changePassword`;
