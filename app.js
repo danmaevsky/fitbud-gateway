66dd10acf3759a57cc32d145f662feabaae3a2a6
danmaevsky <dan.mayevskiy@gmail.com>
2023-03-19 23:18:01 -0400

    express.json() error handling

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use((err, req, res, next) => {
+	// handling JSON error
+	if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+		console.log("Caught error in Gateway:", err.message);
+		return res.status(400).send({ message: "Atrotious request." });
+	}
+	next();
+});
 app.use(
 	cors({
 		origin: "*",
